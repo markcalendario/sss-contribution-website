f3302bf56d3ed7dcc76d990a642b1b32a8326b1f
markcalendario <markcalendario@gmail.com>
2023-06-24 00:52:33 +0800

    refactor: re-structure db schema models

---
+++ server/src/db/configs/contributions.configs.js
@@ -0,0 +1,13 @@
+import configs from "./global.configs.js";
+
+export default {
+  amount: {
+    ...configs.contribution_amount
+  },
+  month: {
+    ...configs.month
+  },
+  year: {
+    ...configs.year
+  }
+};
---
+++ server/src/db/configs/members.configs.js
@@ -0,0 +1,52 @@
+import configs from "./global.configs.js";
+
+export default {
+  sss_no: {
+    ...configs.sss_no
+  },
+  address: {
+    length: 100
+  },
+  zip: {
+    length: 9,
+    type: "number"
+  },
+  tin: {
+    length: 12,
+    min: 12,
+    max: 12
+  },
+  mobile: {
+    length: 11,
+    min: 11,
+    max: 11
+  },
+  telephone: {
+    length: 15,
+    min: 7,
+    max: 15
+  },
+  payorType: {
+    length: 18,
+    allowedValues: [
+      "business",
+      "household",
+      "self-employed",
+      "ofw",
+      "farmer/fisherman",
+      "non-working spouse",
+      "voluntary"
+    ]
+  },
+  email: {
+    length: 100
+  },
+  password: {
+    length: 72, // ignore this in input validation, it's the number of characters in a Bcypt hashing
+    min: 4,
+    max: 16
+  },
+  membership_type: {
+    allowedValues: ["employer", "individual"]
+  }
+};
